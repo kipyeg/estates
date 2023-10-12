1563ac42a52196e6e66dfec3209f2216ee532d15
kipyeg <MY_NAME@example.com>
2023-10-12 11:26:43 +0300

    connecting mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.listen(3001, () => {
   console.log("SERVER STARTED");
 });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
